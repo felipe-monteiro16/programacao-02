a73827faadaa63ba2463335029a89d143e39fa07
Felipe Monteiro <felipemmateuss@gmail.com>
2026-09-17 11:52:40 -0300

    feat(atividade-04): 1 - introdução a formulários em PHP com geração de relatório.

---
+++ atividade-04/01_formulario/recibo.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recibo</title>
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <?php 
+        date_default_timezone_set('America/Sao_Paulo');
+
+        $data_atual = date('d/m/Y'); // Formato: DD/MM/AAAA
+        $hora_atual = date('H:i:s'); // Formato: HH:MM:SS
+
+        $name = $_POST['name'];
+        $phone = $_POST['phone'];
+        $email = $_POST['email'];
+        $text = $_POST['message'];
+    ?>
+    <div class="box">
+        <h1>Comprovante de Envio</h1>
+        <div class="box-group" id="return-box">
+            <p>Nome: <?php print($name) ?></p>
+            <p>Telefone: <?php print($phone) ?></p>
+            <p>E-mail: <?php print($email) ?></p>
+            <p>Mensagem: <?php print($text) ?></p>
+        </div>
+        <p>Mensagem enviada no dia <?php print($data_atual . ", às " . $hora_atual) ?></p>
+
+        <a href="/index.html">Voltar à tela inicial</a>
+    </div>
+</body>
+</html>
